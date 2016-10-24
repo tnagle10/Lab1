c7848c603dca769235ecf260dc8a19c2fa02dd17
Tom Nagle <tnagle10@gmail.com>
2016-10-24 16:18:08 -0400

    Added comments
    Added comments to makes code more understandable

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -9,23 +9,34 @@ namespace Lab1
     class Program
     {
 
-        private static void calcAreaPerimeter(int [] input)
-        {
+        private static int[] getAreaPerimeter(int [] input)
+        {   
+            /* Name: getAreaPerimeter
+             * Description: This method takes a length and width as input, and output area and perimeter
+             * Input: integer array - Length and Width
+             * Output: integer array - Area and Perimeter
+             */
+             
             int length = input[0];
             int width = input[1];
             int area = length * width;
             int perimeter = 2 * (length * width);
-            Console.WriteLine("Area: "+area);
-            Console.WriteLine("Perimter: "+perimeter);
+            int[] outAreaPerimiter = { area, perimeter };
+            return outAreaPerimiter;
 
         }
 
         private static int convToIntWithErrorCheck(string Input)
         {
-            // This method converts a string to decimal using Convert.ToDecimal().
-            // It uses System error checking to make sure the input valid.
-            // If the input is valid, it returns the input converted to decimal.
-            // If the input is invalid, it returns a 0  
+
+            /* Name: convToIntWithErrorCheck
+             * Description:  This method converts a string to integer using Convert.ToInt32().  
+             *               It also implements try and catch error checking
+             * Input: String that needs to be converted to integer
+             * Output: If input string is valid,   returns integer value of string
+             *         If input string is invalid, returns 0
+             */
+           
 
             try
             {
@@ -55,8 +66,16 @@ namespace Lab1
   
         private static int keepGoing()
         {
+            /* Name: keepGoing
+            * Description:  This method implements a loop to determine if users wants to continue
+            * Input:  None
+            * Output: Returns 0 if user doesn't want to continue.  Otherwise returns 1.
+            *         Outputs values to Console
+            */
+
+
             // If user enters "q", execute exit procedure
-            Console.WriteLine("Continue (y/n):");
+            Console.WriteLine("Continue? (y/n):");
             string input = Console.ReadLine();
 
             if (input == "n")
@@ -74,15 +93,22 @@ namespace Lab1
                 return 1;
          
         }
-
-
+        
         private static int [] getLengthWidth()
         {
-           // Get length and width from user in string format
-           Console.WriteLine("Enter Length:");
-           string lengthS = Console.ReadLine();
-           Console.WriteLine("Enter Width:");
-           string widthS = Console.ReadLine();
+            /* Name: getLengthWidth
+            * Description:  This method takes an input Length and Width in integer and 
+            *               returns Area and Permiter integers.  
+            * Input: No input.  Get Length and Width from user input
+            * Output: Integer array of Length and Width
+            */
+
+
+            // Get length and width from user
+            Console.WriteLine("Enter Length:");
+            string lengthS = Console.ReadLine();
+            Console.WriteLine("Enter Width:");
+            string widthS = Console.ReadLine();
 
             //Convert length and width to integer
             int lengthI = convToIntWithErrorCheck(lengthS);
@@ -99,8 +125,16 @@ namespace Lab1
 
             do
             {
+                // Get Length and Width from user and output results in integer array
                 int [] classroomLW =  getLengthWidth();
-                calcAreaPerimeter(classroomLW);
+
+                // Get Area and Perimeter and output results in integer array
+                int [] classroomAP =  getAreaPerimeter(classroomLW);
+
+                // Output Area and Perimiter
+                Console.WriteLine("Area: \n" + classroomAP[0]);
+                Console.WriteLine("Perimeter: \n" + classroomAP[1]);
+                Console.WriteLine("\n");
 
             }
             while (keepGoing() != 0);
